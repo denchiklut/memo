ed179206041a9fb809013bb30edf93f63bb26f8c
Denis Aleksandrov <ollylut@gmail.com>
2022-04-09 09:15:22 +0200

    Initial Commit

---
+++ Shared/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Shared
+//
+//  Created by Denis Aleksandrov on 4/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
