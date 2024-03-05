ffa05667f63061232797d8a1023a93edef186c2b
Karl Aleksandrov <ollylut@gmail.com>
2024-03-05 17:30:21 +0100

    feat: statchart - added chart title

---
+++ Shared/Models/ProgresStat.swift
@@ -0,0 +1,36 @@
+//
+//  ProgresStat.swift
+//  Memo (iOS)
+//
+//  Created by Denis Aleksandrov on 3/5/24.
+//
+
+import Foundation
+
+
+struct ProgresStat: Identifiable {
+    let id = UUID()
+    let date: Date
+    let added: Int
+    let learned: Int
+}
+
+class ProgresStatGenerator {
+    static func generateRandomData(from startDate: Date, to endDate: Date, count: Int) -> [ProgresStat] {
+        var dataset = [ProgresStat]()
+        let calendar = Calendar.current
+        let dateRange = calendar.dateComponents([.day], from: startDate, to: endDate).day ?? 0
+        
+        for _ in 1...count {
+            let randomDaysOffset = Int.random(in: 0...dateRange)
+            let randomDate = calendar.date(byAdding: .day, value: randomDaysOffset, to: startDate)!
+            let randomAdded = Int.random(in: 1...20)
+            let randomLearned = Int.random(in: 1...20)
+            
+            let stat = ProgresStat(date: randomDate, added: randomAdded, learned: randomLearned)
+            dataset.append(stat)
+        }
+        
+        return dataset.sorted { $0.date < $1.date }
+    }
+}
--- Shared/Views/Profile/Stats/Stats.swift
+++ Shared/Views/Profile/Stats/Stats.swift
@@ -23,9 +23,16 @@ struct Stats: View {
         .init(date: Date.from(year: 2023, month: 10, day: 5), added: 1, learned: 8),
         .init(date: Date.from(year: 2023, month: 10, day: 6), added: 1, learned: 11),
         .init(date: Date.from(year: 2023, month: 10, day: 7), added: 5, learned: 9),
-        .init(date: Date.from(year: 2023, month: 10, day: 8), added: 7, learned: 10),
-        .init(date: Date.from(year: 2023, month: 10, day: 9), added: 8, learned: 5),
-        .init(date: Date.from(year: 2023, month: 10, day: 10), added: 1,learned: 16),
+        .init(date: Date.from(year: 2023, month: 10, day: 8), added: 5, learned: 4),
+        .init(date: Date.from(year: 2023, month: 10, day: 9), added: 4, learned: 2),
+        .init(date: Date.from(year: 2023, month: 10, day: 10), added: 6, learned: 1),
+        .init(date: Date.from(year: 2023, month: 10, day: 11), added: 5, learned: 4),
+        .init(date: Date.from(year: 2023, month: 10, day: 12), added: 2, learned: 9),
+        .init(date: Date.from(year: 2023, month: 10, day: 13), added: 2, learned: 9),
+        .init(date: Date.from(year: 2023, month: 10, day: 14), added: 4, learned: 3),
+        .init(date: Date.from(year: 2023, month: 10, day: 15), added: 7, learned: 10),
+        .init(date: Date.from(year: 2023, month: 10, day: 16), added: 8, learned: 5),
+        .init(date: Date.from(year: 2023, month: 10, day: 17), added: 1,learned: 16),
     ]
     
     private var filteredData: [ProgresStat] {
@@ -41,10 +48,24 @@ struct Stats: View {
         
         return sortedData.filter { $0.date >= startDate && $0.date <= endDate }
     }
+    
+    private var dateFormatter: DateFormatter = {
+          let formatter = DateFormatter()
+          formatter.dateFormat = "MMM d, yyyy"
+          return formatter
+      }()
+    
+    private func computeDate(for value: CGFloat, in sortedData: [ProgresStat]) -> Date {
+        guard let firstDate = sortedData.first?.date, let lastDate = sortedData.last?.date else { return Date() }
+        let totalSeconds = lastDate.timeIntervalSince(firstDate)
+        let adjustedSeconds = totalSeconds * Double(value)
+        return firstDate.addingTimeInterval(adjustedSeconds)
+    }
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Stats")
+            Text("\(dateFormatter.string(from: computeDate(for: rangeStart, in: data))) - \(dateFormatter.string(from: computeDate(for: rangeEnd, in: data)))")
+                .fontWeight(.bold)
                 .padding(.bottom)
             
             StatChart(data: filteredData, showAddedData: showAddedData, showLearnedData: showLearnedData)
@@ -64,16 +85,12 @@ struct Stats: View {
     }
 }
 
-struct ProgresStat: Identifiable {
-    let id = UUID()
-    let date: Date
-    let added: Int
-    let learned: Int
-}
 
 
 
 #Preview {
-//    Stats()
-    Profile()
+    Stats()
+//    Profile()
+//        .preferredColorScheme(.dark)
+    
 }
